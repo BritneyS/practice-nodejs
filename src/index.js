2b52e42a3ceb33c4c79a3e9d6eb1bc271d8a2a12
Britney Smith <brit.smith7@gmail.com>
2019-05-05 18:20:53 -0400

    Fix syntax error when using environment variable for port number

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
+const ENV_PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () =>
-    console.log(`Example app listening on port 3000!`),
+app.listen(ENV_PORT, () =>
+    console.log(`Example app listening on port ${ENV_PORT}!`),
 );
