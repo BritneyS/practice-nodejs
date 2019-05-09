e1fd4ab6d2d172d09c23d9f40a62fd294304c69a
Britney Smith <brit.smith7@gmail.com>
2019-05-09 10:04:48 -0400

    Initial Apollo Server setup with schema and resolver defined

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const ENV_PORT = process.env.PORT;
 const app = express();
 
+const schema = gql`
+    type Query {
+        me: User
+    }
+
+    type User {
+        username: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                username: 'Britney Smith',
+            };
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
 app.use(cors());
 
+server.applyMiddleware({ app, path: 'graphql' });
+
+app.listen({ port: ENV_PORT }, () => {
+    console.log(`Apollo Server listening on http://localhost:${ENV_PORT}/graphql`);
+});
+
+/**** Stubs for REST API
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
@@ -14,3 +47,4 @@ app.get('/', (req, res) => {
 app.listen(ENV_PORT, () =>
     console.log(`Example app listening on port ${ENV_PORT}!`),
 );
+****/
